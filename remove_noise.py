a6f33b3178369526b7b455dec788b557e31b8860
Brent Pedersen <bpederse@gmail.com>
2016-05-26 17:12:38 -0600

    update remove noise to multi-family and multi-parent

--- remove_noise.py
+++ remove_noise.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import itertools as it
 from collections import defaultdict
-from toolshed import reader
+import toolshed as ts
 
 def main():
     p = argparse.ArgumentParser()
@@ -12,55 +12,53 @@ def main():
     run(args)
 
 
-def report(block):
-    print '\t'.join(str(s) for s in [block['chrom'], block['start'], block['end'], block['state']])
+def report(block, family_id, parent, keys=['chrom', 'start', 'end', 'state']):
+    print '\t'.join([str(block[k]) for k in keys] + [parent, family_id])
 
 
 def run(args):
     """
-    chrom   start   end     same(1)_diff(2) dad_SSC00369    mom_SSC00470    template_SSC00468   sib_SSC00471
-    1       14932   14933   1               G/A             G/G             G/G                 G/G
-    1       15446   15447   1               A/G             A/A             A/A                 A/A
-    1       16256   16257   1               G/C             G/G             G/C                 G/C
-    1       19917   19918   2               G/C             G/G             G/C                 G/G
-    1       19918   19919   2               G/A             G/G             G/A                 G/G
-    1       19941   19942   1               G/C             G/G             G/C                 G/C
-    1       20155   20156   1               C/T             C/C             C/T                 C/T
-    1       20157   20158   1               A/C             A/A             A/A                 A/A
-    1       20165   20166   1               A/G             A/A             A/A                 A/A
+chrom	start	end	parent	family_id	same(1)_diff(2)	dad	mom	sib1	sib2	global_call_rate	global_depth_1_10_50_90
+1	10477	10478	mom	11348	2	C/C	C/G	C/G	C/C	1.00	11|24|40|60
+1	10488	10489	dad	12290	1	C/T	C/C	C/C	C/C	1.00	10|23|39|55
+1	10491	10492	dad	11006	1	C/T	C/C	C/C	C/C	1.00	7|15|33|51
+1	10491	10492	mom	11258	1	C/C	C/T	C/T	C/T	1.00	7|15|33|51
     """
-    last_block = {}
-    prev = None
-    curr = None
-    for l in reader(args.bedgraph):
-        curr = l
-        if prev is None:
-            last_block['chrom'] = curr['chrom']
-            last_block['start'] = int(curr['start'])
-            last_block['state'] = curr['same(1)_diff(2)']
-            prev = curr
+    last_block = defaultdict(dict)
+    all_prev = {}
+    curr = {}
 
-        else:
-            if curr['chrom'] != prev['chrom']:
-                report(last_block)
+    print "\t".join("chrom start end state parent family_id".split())
+    for parent in ("mom", "dad"):
+        for l in ts.reader(args.bedgraph):
+            if l['parent'] != parent: continue
+            curr = l
+            id = curr['family_id']
+            if all_prev.get(id) is None:
+                last_block[id]['chrom'] = curr['chrom']
+                last_block[id]['start'] = int(curr['start'])
+                last_block[id]['end'] = int(curr['end'])
+                last_block[id]['state'] = curr['same(1)_diff(2)']
+                all_prev[id] = curr
             else:
-                if curr['same(1)_diff(2)'] != prev['same(1)_diff(2)']:
-                    if last_block['end'] - last_block['start'] > args.min_size:
-                        report(last_block)
-                    last_block = {}
-                    last_block['start'] = int(curr['start'])
-                    last_block['chrom'] = curr['chrom']
-                    last_block['state'] = curr['same(1)_diff(2)']
-                    last_block['end'] = int(curr['end'])
+                prev = all_prev[id]
+                if curr['chrom'] != prev['chrom']:
+                    report(last_block[id], id, parent)
                 else:
-                    last_block['end'] = int(curr['end'])
-        prev = curr
-
-    # cleanup of last block
-    report(last_block)
-
-
-
+                    if curr['same(1)_diff(2)'] != prev['same(1)_diff(2)']:
+                        if last_block[id]['end'] - last_block[id]['start'] > args.min_size:
+                            report(last_block[id], id, parent)
+                        last_block[id] = {}
+                        last_block[id]['start'] = int(curr['start'])
+                        last_block[id]['chrom'] = curr['chrom']
+                        last_block[id]['state'] = curr['same(1)_diff(2)']
+                        last_block[id]['end'] = int(curr['end'])
+                    else:
+                        last_block[id]['end'] = int(curr['end'])
+            all_prev[id] = curr
+        # cleanup of last blocks
+        for id, d in last_block.items():
+            report(d, id, parent)
 
 if __name__ == "__main__":
     main()
