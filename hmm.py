73313f67ebbc853ef8edd6cf3840e5d80679058b
Brent Pedersen <bpederse@gmail.com>
2016-05-31 14:51:45 -0600

    handle input

--- hmm.py
+++ hmm.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import numpy as np
+from collections import Counter
 from hmmlearn import hmm
 import sys
 
@@ -26,6 +27,9 @@ def fit(obs, n=20, transmat=np.array([[0.999, 0.001], [0.001, 0.999]])):
 
     """
     aobs = np.asarray(obs)
+    if aobs.max() == 2:
+        aobs -= 1
+    print(Counter(aobs), file=sys.stderr)
 
     obs = aobs.reshape(1, -1).T
     model = hmm.GaussianHMM(n_components=2, n_iter=100,
@@ -40,9 +44,47 @@ def fit(obs, n=20, transmat=np.array([[0.999, 0.001], [0.001, 0.999]])):
     return res
 
 
+def main(args=None):
+
+    import toolshed as ts
+    if args is None:
+        if len(sys.argv) > 1:
+            args = sys.argv[1:]
+        else:
+            args = ["-"]
+
+    rows = []
+    for d in ts.reader(args[0], header='ordered'):
+        if d['start'] == 'start': continue
+        v = int(d['same(1)_diff(2)'])
+        rows.append((d['chrom'], int(d['start']), v, d))
+    rows.sort()
+
+    vals = fit([r[2] for r in rows])
+
+    for i, row in enumerate((r[-1] for r in rows)):
+        if i == 0:
+            print("\t".join(row.keys() + ['hmm-state']))
+        print("%s\t%d" % ("\t".join(row.values()), vals[i]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     import doctest
     res = doctest.testmod()
     if res.failed > 0:
         print(res, file=sys.stderr)
         sys.exit(1)
+    main()
