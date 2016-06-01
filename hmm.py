8e963413f226a3dfcf138c76dab1c77d03538e83
Brent Pedersen <bpederse@gmail.com>
2016-06-01 11:46:38 -0600

    switch to pomegranate

--- hmm.py
+++ hmm.py
@@ -2,47 +2,81 @@ from __future__ import print_function
 import numpy as np
 from collections import Counter
 from hmmlearn import hmm
+import pomegranate as po
 import sys
 
 np.random.seed(42)
 
+eps = 1e-8
 
-def fit(obs, n=20, transmat=np.array([[0.999, 0.001], [0.001, 0.999]])):
+def fit(obs, noise_pct=0.2):
     """
-    >>> obs = [0] * 20 + [0, 1, 0, 1, 0] + [1] * 20
-    >>> fit(obs)
-    array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    #>>> obs = [0] * 200 + [0, 1, 0, 1, 0] + [1] * 200
+    #>>> fit(obs)
+    array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
 
-    >>> obs = [1] * 20 + [0, 1, 0, 1, 0] + [0] * 20
-    >>> fit(obs)
-    array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-           1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-
-    >>> obs = [0] + [1] * 20 + [0, 1, 0, 1, 0] + [0] * 20
-    >>> fit(obs)
-    array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-           1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
+    #>>> obs = [1] * 20 + [0, 1, 0, 1, 0] + [0] * 20
+    #>>> fit(obs)
+    array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    #>>> obs = [0, 1, 0, 1, 0] + [1] * 400
+    #>>> res = fit(obs)
+    #>>> np.all(res == 1)
+    #True
     """
-    aobs = np.asarray(obs)
-    if aobs.max() == 2:
-        aobs -= 1
-    print(Counter(aobs), file=sys.stderr)
+    obs = np.asarray(obs)
+    if obs.max() == 2:
+        obs -= 1
 
-    obs = aobs.reshape(1, -1).T
-    model = hmm.GaussianHMM(n_components=2, n_iter=100,
-            init_params='mcs',
-            params="mcs")
-    model.transmat_ = transmat
-    model.fit(obs)
-    res = model.predict(obs)
+    if noise_pct > 1:
+        noise_pct /= 100.
 
-    if np.abs(res - aobs).mean() > 0.5:
-        res = 1 - res
-    return res
 
+    model = po.HiddenMarkovModel("crossover")
+
+    # allow, e.g. 6% of sites in a true state 1 to appear as state 0
+    di0 = po.DiscreteDistribution({1: 1 - noise_pct, 0: noise_pct})
+    di1 = po.DiscreteDistribution({0: 1 - noise_pct, 1: noise_pct})
+
+    i0 = po.State(di0, name="0")
+    i1 = po.State(di1, name="1")
+
+    model.add_states(i0, i1)
+
+    m = np.mean(obs[:50])
+
+    model.add_transition(model.start, i0, m)
+    model.add_transition(model.start, i1, 1 - m)
+
+    model.add_transition(i0, i0, 1 - eps)
+    model.add_transition(i1, i1, 1 - eps)
+    model.add_transition(i0, i1, eps)
+    model.add_transition(i1, i0, eps)
+
+    model.bake()
+    _, path = model.viterbi(obs)
+    #model.fit(obs)
+    v = np.array([x[0] for x in path[1:]])
+    if np.abs(v - obs).mean() > 0.5:
+        v = 1 - v
+    return v
 
 def main(args=None):
 
@@ -69,22 +103,11 @@ def main(args=None):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     import doctest
     res = doctest.testmod()
+    print(res, file=sys.stderr)
     if res.failed > 0:
-        print(res, file=sys.stderr)
         sys.exit(1)
+
     main()
