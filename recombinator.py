c24c2bfbe6e1697b257fa7e37096e0c35ba115fc
Brent Pedersen <bpederse@gmail.com>
2016-09-13 15:39:54 -0600

    recombinator now does plots and better filtering. calls improved relative to hmm.

--- recombinator.py
+++ recombinator.py
@@ -31,13 +31,11 @@ def main():
         args.prefix = os.path.join(args.prefix, args.region.split(":")[0])
     run(args)
 
-def crossovers(f, min_sites=5):
+def crossovers(f, min_sites=20):
     """
     call the actual crossovers. skip blocks with fewer than min-sites sites and
     then merge the resulting adjacent blocks in the same state
     """
-
-
     fin = gzip.open(f)
     header = next(fin).rstrip().split("\t")
     ids = [h for h in header if h[-1] == "*"]
@@ -60,13 +58,64 @@ def crossovers(f, min_sites=5):
 
         cache[-1].append(d)
         last = d['same']
+    fin.close()
 
     cache[-1] = collapse(cache[-1], parent_id, kid_id)
+    # remove single and double-tons immediately
+    cache = remove_bad_regions(cache)
+    cache = enforce_min_sites(cache, 3)
+    write_crossovers(cache, f.replace('.bed.gz', '.crossovers-unfiltered.bed'))
     cache = enforce_min_sites(cache, min_sites)
+    return write_crossovers(cache, f.replace('.bed.gz', '.crossovers.bed'))
+
+
+def xmean(x, N):
+    """
+    >>> xmean(np.array([1, 2, 3, 2, 1, 600, 2, 10]), 2)
+    array([   2.        ,    2.33333333,    2.        ,  201.        ,
+            201.        ,  204.        ,    6.        ,   10.        ])
+    """
+
+    res = []
+    for i in range(len(x)):
+        res.append(np.mean(x[i: i+N+1]))
+    res = np.array(res)
+    assert res.shape == x.shape
+    return res
+
+
+def remove_bad_regions(cache, n=5):
+    """
+    remove regions where there is a lot of flipping back and forth
+    between states in a short distance.
+    """
+    assert n % 2 == 1
+    for i in range(2):
+        c0 = cache[::2]
+        assert len(set([c['same'] for c in c0])) == 1
+        c1 = cache[1::2]
+        assert len(set([c['same'] for c in c1])) == 1
+
+        vals0 = np.array([d['informative-sites'] for d in c0])
+        vals1 = np.array([d['informative-sites'] for d in c1])
+
+        keep0, = np.where(xmean(vals0, 2) >= n)
+        keep1, = np.where(xmean(vals1, 2) >= n)
+        keep0, keep1 = set(keep0), set(keep1)
+
+        cache = [c for i, c in enumerate(c0) if i in keep0]
+        cache += [c for i, c in enumerate(c1) if i in keep1]
+
+        cache.sort(key=itemgetter('start'))
+        # this merges adjacent blocks
+        cache = enforce_min_sites(cache, 0)
+    return cache
+
+def write_crossovers(cache, fname):
     # now we have starts and ends of the blocks of same state. crossovers occur
     # betwen those blocks.
     if len(cache) < 2: return
-    o = open(f.replace(".bed.gz", ".crossovers.bed"), "w")
+    o = open(fname, "w")
     for i in range(len(cache) - 1):
         s, e = cache[i].copy(), cache[i + 1]
         st = s.pop('same')
@@ -85,8 +134,8 @@ def collapse(dlist, parent_id, kid_id):
     assert len(set(d['same'] for d in dlist)) == 1
     d = OrderedDict([
         ('chrom', dlist[0]['chrom']),
-        ('start', dlist[0]['start']),
-        ('end', dlist[-1]['end']),
+        ('start', int(dlist[0]['start'])),
+        ('end', int(dlist[-1]['end'])),
         ('same', dlist[0]['same']),
         ('family_id', dlist[0]['family_id']),
         ])
@@ -247,7 +296,7 @@ def run(args):
 
     # header
     for i, v in enumerate(vcf_iter, start=1):
-        if i % 50000 == 0:
+        if i % 200000 == 0:
             print("at record %d (%s:%d)" % (i, v.CHROM, v.POS), file=sys.stderr)
         if v.var_type != 'snp':
             if len(v.REF) > 3 or len(v.ALT) > 1 or len(v.ALT[0]) > 3:
@@ -284,29 +333,114 @@ def run(args):
             # detect crossovers.
             for parent, (p1, p2) in [("dad", (0, 1)), ("mom", (1, 0))]:
 
-                if f['gt_type'][p1] == HET and f['gt_type'][p2] == HOM_REF:
-                    if gt_bases is None:
-                        gt_bases = v.gt_bases
-                    fam_bases = "\t".join(gt_bases[f['idxs']])
-                    pctiles = "|".join("%.0f" % v for v in
-                            np.percentile(v.gt_depths, (1, 10, 50, 90)))
+                if not (f['gt_type'][p1] == HET and f['gt_type'][p2] == HOM_REF):
+                    continue
 
-                    val = 1 if f['gt_type'][2] == f['gt_type'][3] else 0
-                    f['fh-%s' % parent].write('\t'.join(str(s) for s in [v.CHROM, v.POS - 1, v.POS,
-                            parent, f['family_id'], val, fam_bases, "%.2f" %
-                            v.call_rate, pctiles]) + '\n')
+                if gt_bases is None:
+                    gt_bases = v.gt_bases
+                fam_bases = "\t".join(gt_bases[f['idxs']])
+                pctiles = "|".join("%.0f" % v for v in
+                        np.percentile(v.gt_depths, (1, 10, 50, 90)))
+
+                val = 1 if f['gt_type'][2] == f['gt_type'][3] else 0
+                f['fh-%s' % parent].write('\t'.join(str(s) for s in [v.CHROM, v.POS - 1, v.POS,
+                        f['ids'][p1], f['family_id'], val, fam_bases, "%.2f" %
+                        v.call_rate, pctiles]) + '\n')
+
     kept = _remove_empty(fs)
     try:
-        from matplotlib.pytplot import plt
+        import matplotlib.pyplot as plt
         import seaborn as sns
+        plot = xplot
     except ImportError:
-        sys.stderr.write("install matpllotlib and seaborn for plots")
+        sys.stderr.write("install matplotlib and seaborn for plots\n")
+        plot = _plot
     for f in kept:
         xos = crossovers(f)
         plot(xos, f)
 
-def plot(fxos, fsites):
-    raise Exception("TODO")
+def _plot(*args, **kwargs):
+    pass
+
+def rdr(f, ordered=False):
+
+    fh = (gzip.open if f.endswith(".gz") else open)(f)
+    header = next(fh).rstrip("\r\n").split("\t")
+    if header[0][0] == "#":
+        header[0] = header[0][1:]
+
+    for toks in (l.rstrip("\r\n").split("\t") for l in fh):
+        if ordered:
+            yield OrderedDict(zip(header, toks))
+        else:
+            yield dict(zip(header, toks))
+    fh.close()
+
+def xplot(fxos, fsites):
+    import matplotlib.pyplot as plt
+    from matplotlib import ticker
+    import seaborn as sns
+    figname = fsites.replace(".bed.gz", ".png")
+
+    fig, ax = plt.subplots(1, sharex=True)
+
+    ax.set_title("crossovers")
+    name = fsites.split("/")[-1].split(".")[0]
+    fig.suptitle(name)
+
+    xs, ys = [], []
+    for row in rdr(fsites):
+        xs.append(int(row['start']))
+        ys.append(int(row['same']))
+
+    ax.plot(xs, ys, 'k.', zorder=5)
+    rng = xs[-1] - xs[0]
+
+    def fmtr(x, p):
+        if xs[-1] - xs[0] > 3000000:
+            v, suf = "%.2f" % (x / 1000000.), "M"
+        else:
+            v, suf = "%.2f" % (x / 1000.), "K"
+        # strip trailing 0's and "."
+        while v[-1] in '.0' and '.' in v:
+            v = v[:-1]
+        return v + suf
+
+    ax.get_xaxis().set_major_formatter(ticker.FuncFormatter(fmtr))
+    ax.set_xlim(xmin=xs[0] - 1, xmax=xs[-1] + 1)
+    ax.set_ylim(ymin=0, ymax=1)
+    ax.set_ylabel('State')
+    ax.set_xlabel('Genomic Position')
+
+    if fxos is None:
+        plt.savefig(figname)
+        return
+
+    left = xs[0]
+    # plot the runs and plot the actual crossovers in red.
+    for row in rdr(fxos):
+        left_state = int(row['change'].split("-")[0])
+        ax.axvspan(xmin=left, xmax=int(row['start']),
+                ymin=left_state / 2., ymax=(left_state+1) / 2.,
+                ec='none')
+        left = int(row['end'])
+
+
+        # plot the actual crossover:
+        ax.axvspan(xmin=int(row['start']), xmax=int(row['end']),
+                color='#ff3333', zorder=4)
+
+        # plot some extra so we're sure we can see it.
+        ax.axvspan(xmin=int(row['start']) - 0.005 * rng, xmax=int(row['end']) +
+                0.005 * rng,
+                color='#ff3333', zorder=4, alpha=0.3)
+
+    # plot the last block
+    right_state = int(row['change'].split("-")[1])
+    ax.axvspan(xmin=left, xmax=xs[-1],
+            ymin=right_state / 2., ymax=(right_state+1) / 2., ec='none')
+
+    plt.savefig(figname)
 
 def _remove_empty(fs):
     # remove empty files.
@@ -362,4 +496,6 @@ def phased_check(fam, v, gt_bases):
             fam['fh-%s-%s' % (parent, kid_id)].flush()
 
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
