a164c2ea5e91debacbe658e85fa38e89ebafad05
Brent Pedersen <bpederse@gmail.com>
2016-10-14 12:44:04 -0600

    commit changes used to call gcs

--- gc.py
+++ gc.py
@@ -1,59 +1,61 @@
+from __future__ import print_function
 import sys
 import toolshed as ts
 import itertools as it
 from operator import itemgetter
 from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Manager
 
 fam_beds = sys.argv[1:]
 
 min_sites = 2
-min_flank = 10
+min_flank = 20
 max_sites = 20
 max_length = 3000
 
 
-print("#chrom\tstart\tend\tsites-left\tsites-in\tsites-right\tparent_id\tfamily_id")
+print("#chrom\tstart\tend\tsites-left\tsites-in\tsites-right\tparent_id\tfamily_id\tab\tdepth\tfile")
 
-def find_gcs(args):
-    fam_bed, lock = args
+def get_ab(grp):
+    ab = []
+    for d in grp:
+        ab.append("|".join(x for x in d['family_allele_balance'].split("|") if x != 'nan'))
+    return ",".join(ab)
+
+def find_gcs(fam_bed):
     last_grp = None
     ends = []
     nsites = []
-    try:
-        for _, grp in it.groupby(ts.reader(fam_bed, header="ordered"), itemgetter('same')):
+    res = []
 
-            grp = list(grp)
-            ends.append(int(grp[-1]['end']))
-            nsites.append(len(grp))
-            # cant find until we have at least 3 groups.
-            if len(ends) < 3:
-                last_grp = grp
-                continue
+    for _, grp in it.groupby(ts.reader(fam_bed), itemgetter('same')):
 
-            start = int(grp[0]['start'])
-            # check num sites, distance between flanking regions and number of
-            if min_sites <= len(last_grp) <= max_sites and (start - ends[-3]) < max_length and nsites[-1] >= min_flank and nsites[-3] >= min_flank:
-                d = grp[0]
-
-                ab_fail = False
-                for d in grp:
-                    if any(not 0.40 < float(v) < 0.60 for v in d['family_allele_balance'].split("|") if v != "nan"):
-                        ab_fail = True
-                        break
-
-                if not ab_fail:
-                    lock.acquire()
-                    print("\t".join(map(str, (d['chrom'], ends[-3], start, nsites[-3],
-                        nsites[-2], nsites[-1], d['parent_id'], d['family_id'], fam_bed))))
-                    lock.release()
-                assert nsites[-2] == len(last_grp)
-
-            # set it here, but still need to check if it's bounds are small enough.
+        grp = list(grp)
+        ends.append(int(grp[-1]['end']))
+        nsites.append(len(grp))
+        # cant find until we have at least 3 groups.
+        if len(ends) < 3:
             last_grp = grp
-    except:
-        pass
+            continue
 
-with ProcessPoolExecutor() as p:
-    l = Manager().Lock()
-    list(p.map(find_gcs, ((f, l) for f in fam_beds)))
+        start = int(grp[0]['start'])
+        # check num sites, distance between flanking regions and number of
+        if min_sites <= len(last_grp) <= max_sites and (start - ends[-3]) < max_length and nsites[-1] >= min_flank and nsites[-3] >= min_flank:
+            d = last_grp[0]
+
+            res.append("\t".join(map(str, (d['chrom'], ends[-3], start, nsites[-3],
+                    nsites[-2], nsites[-1], d['parent_id'], d['family_id'],
+                    get_ab(last_grp),
+                    ",".join(x['family_depth'] for x in last_grp),
+                    fam_bed))))
+            assert nsites[-2] == len(last_grp)
+
+        # set it here, but still need to check if it's bounds are small enough.
+        last_grp = grp
+
+    return res
+
+with ProcessPoolExecutor(10) as p:
+    for lines in p.map(find_gcs, (f for f in fam_beds)):
+        if len(lines) > 0:
+            print("\n".join(lines))
+            sys.stdout.flush()
